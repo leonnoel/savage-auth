208edcb540ab65261be2bf06a9a0cf0768400ece
Leon Noel <leon.a.noel+github@gmail.com>
2018-03-15 13:54:31 -0400

    added database.js config

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://derp:derp1@ds215019.mlab.com:15019/auth' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
