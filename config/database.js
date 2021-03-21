3e55af8ea9dfd86a0b94172ed5fffb22b6f1a4b0
Jasmin Alvarez <70730021+Jasmin-L-Alvarez@users.noreply.github.com>
2021-03-21 03:16:45 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@
 module.exports = {
 
     // 'url' : 'mongodb+srv://demo:demo@cluster0-92dvh.mongodb.net/test?retryWrites=true', 
-     'url':'mongodb+srv://savage:123@cluster0.gfz1v.mongodb.net/demo?retryWrites=true&w=majority',
+     'url':'mongodb+srv://savage:123@cluster0.gfz1v.mongodb.net/auth?retryWrites=true&w=majority',
     'dbName': 'auth'
 };
