e681014c95d497dcacc650a3ac30786a78646393
Franklin WIllis Conover II <franklin.conoverII@outlook.com>
2022-05-29 16:36:11 -0400

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
-    'dbName': 'demo'
+    'url': 'mongodb+srv://fwcii:qljZ1RXKbwBWKOep@cluster0.vje6f.mongodb.net/savageauth?retryWrites=true&w=majority', 
+    'dbName': 'messages'
 };
