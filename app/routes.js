d6db085624e9604014cac11b9480958fb8119a45
Denzel M. Echevarria <denzel.echevarria@gmail.com>
2023-04-27 13:17:16 -0400

    fixed the thumbs down

--- app/routes.js
+++ app/routes.js
@@ -51,6 +51,21 @@ module.exports = function(app, passport, db) {
       })
     })
 
+    app.put('/thumbDown', (req, res) => {
+      db.collection('messages')
+      .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+        $set: {
+          thumbUp:req.body.thumbUp - 1
+        }
+      }, {
+        sort: {_id: -1},
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+    })
+
     app.delete('/messages', (req, res) => {
       db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
         if (err) return res.send(500, err)
--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var thumbDown = document.getElementsByClassName("fa-thumbs-down");
 var trash = document.getElementsByClassName("fa-trash");
 
 Array.from(thumbUp).forEach(function(element) {
@@ -23,6 +24,30 @@ Array.from(thumbUp).forEach(function(element) {
           window.location.reload(true)
         })
       });
+});
+
+Array.from(thumbDown).forEach(function(element) {
+  element.addEventListener('click', function(){
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const msg = this.parentNode.parentNode.childNodes[3].innerText
+    const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+    fetch('thumbDown', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'name': name,
+        'msg': msg,
+        'thumbUp':thumbUp
+      })
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+  });
 });
 
 Array.from(trash).forEach(function(element) {
